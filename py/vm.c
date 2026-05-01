5ded8a04e515bf5a80fba3380066405aba70085f
Pavel Durov <pavel@p3ld3v.dev>
2026-05-01 10:09:04 +0100

    Remove yk_promote from MP_BC_LOAD_CONST_OBJ.
    Calling yk_promote() in MP_BC_LOAD_CONST_OBJ adds runtime overhead of
    ~30% on AWFY List benchmark.

--- py/vm.c
+++ py/vm.c
@@ -425,11 +425,7 @@ dispatch_loop:
 
                 ENTRY(MP_BC_LOAD_CONST_OBJ): {
                     DECODE_OBJ;
-#ifdef USE_YK
-                    PUSH(yk_promote(obj));
-#else
                     PUSH(obj);
-#endif
                     DISPATCH();
                 }
 
